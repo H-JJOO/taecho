e6706a1256c3dba6e8e102f1e45f6d610ff58682
Joo HyeonJin <icepri3535@gmail.com>
2022-11-15 10:23:53 +0900

    1st test

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port = 5000, debug = True)
