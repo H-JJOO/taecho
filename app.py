691c18ecca8d01b0e18198b974fc8e61282ce20e
befunc <jsb22c@naver.com>
2022-11-16 16:48:08 +0900

    blog cm

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.1iypvdi.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
 
 # index 페이지
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 # 팀 소개 페이지
 @app.route('/introduce')
 def introduce():
     return render_template('introduce.html')
+
 
 # 팀원 한명 한명 페이지
 @app.route('/member_1')
 def joo():
     return render_template('member_1.html')
 
+
 @app.route('/member_2')
 def jun():
     return render_template('member_2.html')
 
+
 @app.route('/member_3')
 def park():
     return render_template('member_3.html')
+
 
 @app.route('/member_4')
 def lee():
@@ -47,6 +57,8 @@ def web_board_post():
 @app.route('/member_5')
 def jung():
     return render_template('member_5.html')
+
+
 # 팀원 한명 한명 페이지
 
 # 팀 약속 페이지
@@ -54,10 +66,23 @@ def jung():
 def promise():
     return render_template('promise.html')
 
+
 # 블로그 페이지
+@app.route('/')
+def home():
+    return render_template('blog.html')
+
+
+@app.route("/bc", methods=["GET"])
+def blog_get():
+    blog_list = list(db.TaechoBlog.find({}, {'_id': False}))
+    return jsonify({'bc': blog_list})
+
+
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
+
 
 # 방명록 페이지
 @app.route('/board')
@@ -66,4 +91,4 @@ def board():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port = 5000, debug = True)
+    app.run('0.0.0.0', port=5000, debug=True)
