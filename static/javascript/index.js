f515f23b16d16da9704d95ce705043fad0f5b8f3
Lee HyeWon <lulilaks7@gmail.com>
2022-11-16 20:27:45 +0900

    show_order() function 지움

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -6,24 +6,3 @@ function close_book() {
     $('#book-box').hide()
 }
 
-function write_book() {
-
-    let name = $('#floatingInput').val()
-    let message = $('#floatingTextarea2').val()
-
-    if (name === '') {
-        alert('이름을 써주세요!')
-    }
-    if (message === '') {
-        alert('메세지를 남겨주세요!')
-    } else $.ajax({
-        type: 'POST',
-        url: '/name',
-        data: {name_give: name, message_give: message},
-        success: function (response) {
-            alert(response['msg'])
-            window.location.reload()
-        }
-    });
-
-}
