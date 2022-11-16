d38f51e1a6ca2e593eb88c19e339853758aa0a30
Lee HyeWon <lulilaks7@gmail.com>
2022-11-16 20:28:32 +0900

    드디어 POST랑 GET 모든 자기소개 페이지에서 방명록 페이지로 연결됨

--- app.py
+++ app.py
@@ -22,13 +22,55 @@ def introduce():
 def joo():
     return render_template('member_1.html')
 
+@app.route("/member_1", methods=["POST"])
+def post_guest_list1():
+
+    name_receive = request.form['name_give']
+    message_receive = request.form['message_give']
+
+    doc = {
+        'name': name_receive,
+        'message': message_receive
+    }
+    db.member_1.insert_one(doc)
+
+    return jsonify({'msg': '기록 완료!'})
+
 @app.route('/member_2')
 def jun():
     return render_template('member_2.html')
 
+@app.route("/member_2", methods=["POST"])
+def post_guest_list2():
+
+    name_receive = request.form['name_give']
+    message_receive = request.form['message_give']
+
+    doc = {
+        'name': name_receive,
+        'message': message_receive
+    }
+    db.member_2.insert_one(doc)
+
+    return jsonify({'msg': '기록 완료!'})
+
 @app.route('/member_3')
 def park():
     return render_template('member_3.html')
+
+@app.route("/member_3", methods=["POST"])
+def post_guest_list3():
+
+    name_receive = request.form['name_give']
+    message_receive = request.form['message_give']
+
+    doc = {
+        'name': name_receive,
+        'message': message_receive
+    }
+    db.member_3.insert_one(doc)
+
+    return jsonify({'msg': '기록 완료!'})
 
 @app.route('/member_4')
 def lee():
@@ -36,7 +78,7 @@ def lee():
 
 
 @app.route("/member_4", methods=["POST"])
-def post_guest_list():
+def post_guest_list4():
 
     name_receive = request.form['name_give']
     message_receive = request.form['message_give']
@@ -53,6 +95,20 @@ def post_guest_list():
 @app.route('/member_5')
 def jung():
     return render_template('member_5.html')
+
+@app.route("/member_5", methods=["POST"])
+def post_guest_list5():
+
+    name_receive = request.form['name_give']
+    message_receive = request.form['message_give']
+
+    doc = {
+        'name': name_receive,
+        'message': message_receive
+    }
+    db.member_5.insert_one(doc)
+
+    return jsonify({'msg': '기록 완료!'})
 # 팀원 한명 한명 페이지
 
 # 팀 약속 페이지
@@ -70,12 +126,35 @@ def blog():
 def board():
     return render_template('board.html')
 
-@app.route("/boards", methods=["GET"])
-def get_guest_list():
+@app.route("/board_1", methods=["GET"])
+def get_guest_list1():
+    guest_list1 = list(db.member_1.find({}, {'_id': False}))
+    return jsonify({'guests1': guest_list1})
 
-    guest_list4 = list(db.member_4.find({}, {'_id': False}))
+@app.route("/board_2", methods=["GET"])
+def get_guest_list2():
+    guest_list2 = list(db.member_2.find({}, {'_id': False}))
+    return jsonify({'guests2': guest_list2})
 
-    return jsonify({'guests4': guest_list4})
+@app.route("/board_3", methods=["GET"])
+def get_guest_list3():
+    guest_list3 = list(db.member_3.find({}, {'_id': False}))
+    return jsonify({'guests3': guest_list3})
+
+@app.route("/board_4", methods=["GET"])
+def get_guest_list4():
+    guest_list = list(db.member_4.find({}, {'_id': False}))
+    return jsonify({'guests4': guest_list})
+
+
+@app.route("/board_5", methods=["GET"])
+def get_guest_list5():
+    guest_list5 = list(db.member_5.find({}, {'_id': False}))
+    return jsonify({'guests5': guest_list5})
+
+
+
+
 
 
 if __name__ == '__main__':
