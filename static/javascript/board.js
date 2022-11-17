f12507f7bdd7da218659d48c763105cd2e8404f4
Park Jin <qkrwls6521@gmail.com>
2022-11-17 10:24:18 +0900

    add: comment to board.js

--- static/javascript/board.js
+++ static/javascript/board.js
@@ -1,3 +1,28 @@
+// 방명록 관련 Ajax 함수를 모아놓은 장소
+// 이 js 파일을 개인 페이지에서 불러오고 requestGetBoards 함수를 써주시면 됩니다.
+// 아마 방명록 작성에 개인 작성 함수(ex. create)를 script에 작성하여 그 안에 이 함수를 호출해주면 됩니다.
+
+/**
+ * 예시코드
+ *
+ * function create() {
+ *      const name = $("이름 입력칸의 selector").val();
+ *      const message = ${"내용 입력칸의 selector"}.val();
+ *
+ *      const memberId = 3  <== 박진의 Id
+ *      const data = {
+ *          name: name,
+ *          message: message
+ *      }
+ *
+ *      requestPostBoards(memberId, data, (response) => {
+ *          alert(response['msg'])
+ *          원하면 window.location.reload()
+ *      })
+ *
+ * }
+ */
+
 /**
  *
  * 서버에 방명록 목록을 요청하는 Ajax 함수
